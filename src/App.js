c519daab22c15234d366b189458e1b5447aa30e6
busra <kocarslanbusrab@gmail.com>
2024-04-05 18:05:07 +0300

    header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from './pages/Home';
 function App() {
   return (
     <div className="App">
+      <h1 style={{color:"purple", marginTop:"5rem",textAlign:"center"}}>RANDOM USER</h1>
       <User/>
      
     </div>
